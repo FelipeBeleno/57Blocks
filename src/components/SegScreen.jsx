dfb221f5ab9fdff5a6aa4440fef8787b252f69bf
Felipe} <felipebelenoo1101@gmail.com>
2021-08-18 21:15:44 -0500

    three commit

--- src/components/SegScreen.jsx
+++ src/components/SegScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const SegScreen = () => {
     return (
         <div>
-            <h1>SegPa</h1>
+            <h1>Second Page</h1>
             
         </div>
     )
